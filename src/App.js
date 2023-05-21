c1c4098192f3bfc8a2e70b78d79529fdb8c873a7
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-05-21 21:57:29 +0430

    small changes for Header style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './pages/Home/Home';
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    //      ****** Default page ******
+    navigate('/');
+  }, []);
+
   return (
     <>
-      {/* ******** main-head ***** */}
+      {/* *********** main-head ***** */}
       <Header />
-      <Routes>
-        <Route path="/" element={<h3>Home page</h3>} />
-        <Route path="/page-2" element={<h3>Page-2</h3>} />
-      </Routes>
+
+      {/* *********** content for main ****** */}
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/page-2" element={<h3>Page-2</h3>} />
+        </Routes>
+      </main>
     </>
   );
 }
