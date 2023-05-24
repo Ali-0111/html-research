b0a67c668b8e9124d1b1a2569cb045b852c48532
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-05-24 18:09:53 +0430

    Structure of data changed to arrange better

--- src/util/dataSet.js
+++ src/util/dataSet.js
@@ -2,26 +2,20 @@ const dataSet = [
   {
     tagName: '<h1>...</h1>',
     attributes: 'NA',
-    Usage: {
-      info: 'There are six headings available in HTML,h1 is the largest among all and h6 is the smalles',
-      code: '<p>Codes will be here</p>',
-    },
+    info: 'There are six headings available in HTML,h1 is the largest among all and h6 is the smalles',
+    code: '<p>Codes will be here</p>',
   },
   {
     tagName: '<h1>...</h1>',
     attributes: 'NA',
-    Usage: {
-      info: 'There are six headings available in HTML,h1 is the largest among all and h6 is the smalles',
-      code: '<p>Codes will be here</p',
-    },
+    info: 'There are six headings available in HTML,h1 is the largest among all and h6 is the smalles',
+    code: '<p>Codes will be here</p',
   },
   {
     tagName: '<h1>...</h1>',
     attributes: 'position,style,margin,position,style,margin',
-    Usage: {
-      info: 'There are six headings available in HTML,h1 is the largest among all and h6 is the smalles',
-      code: '<p>Codes will be here</p',
-    },
+    info: 'There are six headings available in HTML,h1 is the largest among all and h6 is the smalles',
+    code: '<p>Codes will be here</p',
   },
 ];
 export default dataSet;
