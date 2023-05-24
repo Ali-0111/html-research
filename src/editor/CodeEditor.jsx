4be542492f96ca0b0acc4626ed7d4b9f54668874
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-05-24 18:33:02 +0430

    Css style changed for codeEditor

--- src/editor/CodeEditor.jsx
+++ src/editor/CodeEditor.jsx
@@ -3,7 +3,7 @@ import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
-import 'prismjs/themes/prism-funky.css';
+import 'prismjs/themes/prism.css';
 
 export default function CodeEditor() {
   const [code, setCode] = React.useState(
