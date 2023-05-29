1214111da5c7b3d7ef826f40b3ee7a0897e1cadd
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-05-30 03:19:50 +0430

    app changes added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import Header from './components/Header';
-import Routing from './components/Navigation/Routing';
-import { feedDataSet } from './Redux/codeSlice';
-import dataSet from './util/dataSet';
-
 function App() {
-  const store = useSelector((store) => (store));
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  useEffect(() => {
-    // ****** Default page ******
-    navigate('/');
-    dispatch(feedDataSet(dataSet));
-  }, []);
-  useEffect(() => {
-    // ****** Default page ******
-    console.log(store.filteredBySearch);
-  }, [store]);
   return (
-    <>
-      {/* *********** main-head ***** */}
-      <Header />
-      {/* *********** content for main ****** */}
-
-      <main>
-        <Routing />
-      </main>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h3 className="font-bold text-lg font-medium transition-colors">hi</h3>
+        <div className="bg-">btn</div>
+      </header>
+    </div>
   );
 }
 
